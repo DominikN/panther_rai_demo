d136a60869e3a6fc681daabe90d5c6af4e825d90
dominikn <dominik.nwk@gmail.com>
2024-10-04 12:56:31 +0200

    wip

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # panther_rai_demo
+
+## How `src/panther_whoami` was created?
+
+https://github.com/RobotecAI/rai/blob/development/docs/create_robots_whoami.md
+
+## Quick Start
+
+```bash
+docker compose build
+docker compose up
+```
--- src/panther_whoami/setup.py
+++ src/panther_whoami/setup.py
@@ -3,29 +3,32 @@ import os
 
 from setuptools import find_packages, setup
 
-package_name = "panther_whoami"
+package_name = 'panther_whoami'
 
 setup(
     name=package_name,
-    version="0.0.0",
-    packages=find_packages(exclude=["test"]),
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
     data_files=[
+
         (os.path.join("share", package_name, os.path.dirname(p)), [p])
         for p in glob.glob("description/**/*", recursive=True)
         if os.path.isfile(p)
     ]
     + [
-        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
-        ("share/" + package_name, ["package.xml"]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=["setuptools"],
+    install_requires=['setuptools'],
     zip_safe=True,
-    maintainer="wsiekierska",
-    maintainer_email="wiktoria.siekierska@robotec.ai",
-    description="TODO: Package description",
-    license="Apache-2.0",
-    tests_require=["pytest"],
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
     entry_points={
-        "console_scripts": [],
+        'console_scripts': [
+        ],
     },
 )
